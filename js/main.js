911aa7b02b36b918626ed2f033bc6c4dd27d4b44
Jens Schadron <jensschadron@hotmail.com>
2017-02-10 00:03:15 +0100

    Toevoegen speech recognition en speech synthesis

--- js/main.js
+++ js/main.js
@@ -13,12 +13,16 @@ window.onload = function () {
     $('#server').text(SERVER_DOMAIN);
     $('#token').text(ACCESS_TOKEN);
 
+    SpeechSetup();
+
     app = new App();
     app.open();
+
+    addResponseNode("How can I be of any help?");
 };
 
 function App() {
-    var apiAi;
+    var apiAi, apiAiTts;
     var sessionId = ApiAi.generateRandomId();
 
     this.sendJson = function () {
@@ -27,14 +31,13 @@ function App() {
                 "v": SERVER_VERSION,
                 "query": query,
                 "timezone": "GMT+1",
-                "lang": "nl",
+                "lang": "en",
 
                 "sessionId": sessionId
             };
         userInput.val(''); //Userinput terug leegmaken
         console.log('sendJson', queryJson);
         apiAi.sendJson(queryJson);
-
     };
 
     this.open = function () {
@@ -44,7 +47,6 @@ function App() {
 
     this.close = function () {
         console.log('close');
-        // apiAi.close();
     };
 
     init();
@@ -55,7 +57,7 @@ function App() {
             serverVersion: SERVER_VERSION,
             token: ACCESS_TOKEN, // Use Client access token there (see agent keys).
             sessionId: sessionId,
-            lang: 'nl',
+            lang: 'en',
             onInit: function () {
                 console.log("> ON INIT use config");
             }
@@ -82,24 +84,8 @@ function App() {
 
             var status = data.status, code = status.code;
 
-            var userQueryContainer = document.createElement("div"),
-                userQuery = document.createElement("p");
-            userQueryContainer.className = "userQueryContainer";
-            userQuery.className = "dialogueMessage userQuery";
-            userQuery.innerHTML = data.result.resolvedQuery;
-
-            userQueryContainer.appendChild(userQuery);
-            dialogue.append(userQueryContainer);
-
-            var apiResponseContainer = document.createElement("div"),
-                apiResponse = document.createElement("p");
-            apiResponseContainer.className = "apiResponseContainer";
-            apiResponse.className = "dialogueMessage apiResponse";
-            apiResponse.innerHTML = (data.result.fulfillment) ? data.result.fulfillment.speech : data.result.speech;
-
-            apiResponseContainer.appendChild(apiResponse);
-            dialogue.append(apiResponseContainer);
-            $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+            addQueryNode(data.result.resolvedQuery);
+            addResponseNode((data.result.fulfillment) ? data.result.fulfillment.speech : data.result.speech);
         };
 
         apiAi.onError = function (code, data) {
@@ -110,7 +96,87 @@ function App() {
         apiAi.onEvent = function (code, data) {
             console.log("> ON EVENT", code, data);
         };
+    }
+}
 
-        // apiAi.init();
+function addQueryNode(text) {
+    var userQueryContainer = document.createElement("div"),
+        userQuery = document.createElement("p");
+    userQueryContainer.className = "userQueryContainer";
+    userQuery.className = "dialogueMessage userQuery";
+    userQuery.innerHTML = text;
+
+    userQueryContainer.appendChild(userQuery);
+    dialogue.append(userQueryContainer);
+}
+
+function addResponseNode(text) {
+    var apiResponseContainer = document.createElement("div"),
+        apiResponse = document.createElement("p");
+    apiResponseContainer.className = "apiResponseContainer";
+    apiResponse.className = "dialogueMessage apiResponse";
+    apiResponse.innerHTML = text;
+
+    apiResponseContainer.appendChild(apiResponse);
+    dialogue.append(apiResponseContainer);
+    $("html, body").animate({scrollTop: $(document).height()}, "slow");
+
+    if ('speechSynthesis' in window) {
+        speech.text = text;
+        speechSynthesis.speak(speech);
+    }
+}
+
+var recognition,
+    speech,
+    recognizing = false;
+
+function SpeechSetup() {
+    if (!('webkitSpeechRecognition' in window)) {
+        console.log("not supported or something like that");
+    } else {
+        recognition = new webkitSpeechRecognition();
+        recognition.continuous = false;
+        recognition.interimResults = true;
+        recognition.lang = 'en-US';
+
+        recognition.onstart = function () {
+            console.log("Starting recog");
+            recognizing = true;
+        };
+        recognition.onresult = function (event) {
+            userInput.val('');
+
+            for (var i = event.resultIndex; i < event.results.length; ++i) {
+                if (event.results[i].isFinal) {
+                    userInput.val(userInput.val() + event.results[i][0].transcript);
+                    console.log("final transcript: " + event.results[i][0].transcript);
+                } else {
+                    userInput.val(userInput.val() + event.results[i][0].transcript);
+                    console.log("interim transcript: " + event.results[i][0].transcript);
+                }
+            }
+        };
+        recognition.onerror = function (event) {
+            console.log("Recog errorred");
+            recognizing = false;
+        };
+        recognition.onend = function () {
+            console.log("Ending recog");
+            recognizing = false;
+            app.sendJson();
+        }
+    }
+
+    if ('speechSynthesis' in window) {
+        speech = new SpeechSynthesisUtterance();
+        var voices = window.speechSynthesis.getVoices();
+        speech.voice = voices[2]; // Note: some voices don't support altering params
+        speech.lang = 'en-US';
+
+        speech.onend = function (e) {
+            console.log("Finished speaking, enabling speech recognition again");
+            recognition.start();
+        };
     }
 }
